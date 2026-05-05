d91f131879bf1b58cbbd35d4e98f1d27cd7d6f59
James Stevens <jas812000@gmail.com>
2026-05-05 20:50:11 +0000

    AB#126 - Initial implementation of pet boarding app: add controllers and views for bookings, pets, users admin, contact, hours and manage bookings. (#1)
    Reviewed-on: https://git.websurfingstudios.com/Phase1/PetBoarding_JamesStevens/pulls/1
    Reviewed-by: Aidan Fahey <afahey03@noreply.websurfingstudios.com>

---
+++ WebAppTemplate/Models/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace WebAppTemplate.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+
+        public DbSet<ToDoModel> ToDoModels { get; set; }
+
+        public DbSet<StudentModel> StudentModels { get; set; }
+
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+
+        
+    }
+}
---
+++ WebAppTemplate/Models/ToDoModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebAppTemplate.Models
+{
+    public class ToDoModel
+    {
+        [Key]
+        public Guid ToDoID { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [MaxLength(1000)]
+        public string Description { get; set; }
+
+        public bool IsComplete { get; set; }
+    }
+}
